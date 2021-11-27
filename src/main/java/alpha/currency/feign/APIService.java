287f05bda922d1b1646913904dc86cbe7983622a
nikulin-me <itdetectiv@gmail.com>
2021-11-27 20:03:42 +0400

    Getting FeignClient

--- src/main/java/alpha/currency/feign/APIService.java
+++ src/main/java/alpha/currency/feign/APIService.java
@@ -3,9 +3,10 @@ package alpha.currency.feign;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
-@FeignClient(name = "api-service")
+@FeignClient(name = "api-service",url = "https://restcountries.com")
 public interface APIService {
 
     @GetMapping("/v3.1/all")
