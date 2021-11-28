7a7984cd74307dc51879601c9aa501e2baa35174
nikulin-me <itdetectiv@gmail.com>
2021-11-28 23:26:38 +0400

    Deleted useless classes V2

--- src/main/java/alpha/currency/model/Gif.java
+++ src/main/java/alpha/currency/model/Gif.java
@@ -6,8 +6,14 @@ import java.util.List;
 import javax.annotation.Generated;
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Generated("jsonschema2pojo")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Gif {
 
     @SerializedName("data")
@@ -20,49 +26,6 @@ public class Gif {
 
     public void setData(List<Datum> data) {
         this.data = data;
-    }
-
-
-
-    @Override
-    public String toString() {
-        StringBuilder sb = new StringBuilder();
-        sb.append(Gif.class.getName()).append('@').append(Integer.toHexString(System.identityHashCode(this))).append('[');
-        sb.append("data");
-        sb.append('=');
-        sb.append(((this.data == null)?"<null>":this.data));
-        sb.append(',');
-        sb.append("pagination");
-        sb.append('=');
-        sb.append(',');
-        sb.append("meta");
-        sb.append('=');
-        sb.append(',');
-        if (sb.charAt((sb.length()- 1)) == ',') {
-            sb.setCharAt((sb.length()- 1), ']');
-        } else {
-            sb.append(']');
-        }
-        return sb.toString();
-    }
-
-    @Override
-    public int hashCode() {
-        int result = 1;
-        result = ((result* 31)+((this.data == null)? 0 :this.data.hashCode()));
-        return result;
-    }
-
-    @Override
-    public boolean equals(Object other) {
-        if (other == this) {
-            return true;
-        }
-        if ((other instanceof Gif) == false) {
-            return false;
-        }
-        Gif rhs = ((Gif) other);
-        return (((this.data == rhs.data)||((this.data!= null)&&this.data.equals(rhs.data))));
     }
 
 }
