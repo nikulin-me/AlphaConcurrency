a8473b7c55d71db088e1b8edae18c60a558ca7f9
nikulin-me <itdetectiv@gmail.com>
2021-11-29 21:31:12 +0400

    Tested GifController

--- src/test/java/alpha/currency/controller/CurrencyControllerTest.java
+++ src/test/java/alpha/currency/controller/CurrencyControllerTest.java
@@ -8,8 +8,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.hamcrest.Matchers.equalTo;
-import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
--- src/test/java/alpha/currency/controller/GifControllerTest.java
+++ src/test/java/alpha/currency/controller/GifControllerTest.java
@@ -1,14 +1,52 @@
 package alpha.currency.controller;
 
+import alpha.currency.model.Datum;
+import alpha.currency.model.Gif;
+import alpha.currency.service.currency.CurrencyService;
+import alpha.currency.service.gif.GifSender;
+import ch.qos.logback.core.net.ObjectWriter;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.google.gson.Gson;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.hamcrest.Matchers.equalTo;
+import static org.mockito.ArgumentMatchers.anyList;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WebMvcTest
+@SpringBootTest
+@AutoConfigureMockMvc
 class GifControllerTest {
 
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private GifSender gifSender;
+
+
     @Test
-    void getGif() {
+    void getGif() throws Exception {
+        Datum datum = new Datum();
+        when(gifSender.getGifByCurrency("RUB"))
+                .thenReturn(new Datum());
+        mockMvc.perform(get("/gif/RUB/json"))
+                .andDo(print())
+                .andExpect(status().is2xxSuccessful())
+                .andExpect(content().contentType("application/json"))
+                .andExpect(content().json(convertObjectToJsonString(datum)));
+
+    }
+    private String convertObjectToJsonString(Datum datum) throws JsonProcessingException {
+        return new Gson().toJson(datum);
     }
 }
